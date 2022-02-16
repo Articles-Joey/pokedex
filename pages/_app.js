5f87a6d234032b09c52cbbe3038294f91de13674
Joey Giusto <joegiusto@outlook.com>
2022-02-16 05:48:30 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import "bootstrap/scss/bootstrap.scss"
+
+import '../styles/index.scss'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
